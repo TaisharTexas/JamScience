b2f5a5cdd158f4639b275b40b19023c96cf0517f
Andrew <bayledgeacademy@icloud.com>
2025-03-20 10:45:38 -0500

    no message

--- download_data.py
+++ download_data.py
@@ -5,9 +5,9 @@ import os
 DATASET_DIR = os.path.expanduser("~/.cache/kagglehub/datasets/robikscube/flight-delay-dataset-20182022")
 
 # Check if dataset already exists
-if not os.path.exists(DATASET_DIR):
-    print("Dataset not found. Downloading...")
-    path = kagglehub.dataset_download("robikscube/flight-delay-dataset-20182022")
-    print(f"Dataset downloaded to: {path}")
-else:
-    print(f"Dataset already exists at: {DATASET_DIR}")
+# if not os.path.exists(DATASET_DIR):
+#     print("Dataset not found. Downloading...")
+#     path = kagglehub.dataset_download("robikscube/flight-delay-dataset-20182022")
+#     print(f"Dataset downloaded to: {path}")
+# else:
+#     print(f"Dataset already exists at: {DATASET_DIR}")
